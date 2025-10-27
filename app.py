97ac9dc581e15a4ecd23f6026e4d200b744075d8
idk19374 <ceiron962@gmail.com>
2025-10-27 12:09:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,7 @@ def text_to_morse():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
